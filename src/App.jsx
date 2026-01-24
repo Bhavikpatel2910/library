d21d52bc08ab50fb9a79e5e403a2b9fb04e03edc
Bhavik Patel <bahvikpatel2550@gmail.com>
2026-01-24 19:55:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Card from './components/Card'
+
+export const App = () => {
+  return (
+  <div>
+    <Navbar />
+    <Card />
+   </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <div  className='card'>
+      <h1>bhavik patel</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, beatae iste quo odit amet fuga enim neque, ratione, eum voluptatibus perferendis! Doloremque, voluptate odio ducimus obcaecati fuga cum quibusdam iure?</p>
+    </div>
+  )
+}
+
+export default Card
